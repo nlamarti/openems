5de7dab10ee00eb34ae0d55557a8b37e9dc3495c
Naoufal Lamarti <naoufal.lamarti@ibood.com>
2023-08-11 16:30:40 +0200

    fix: Add unit test, fix code style

--- io.openems.edge.ess.socomec/src/io/openems/edge/ess/socomec/Control.java
+++ io.openems.edge.ess.socomec/src/io/openems/edge/ess/socomec/Control.java
@@ -6,72 +6,96 @@ import io.openems.edge.bridge.modbus.api.ElementToChannelConverter;
 import io.openems.edge.bridge.modbus.sunspec.DefaultSunSpecModel;
 
 public enum Control implements OptionsEnum {
-    OFF(0, "Off"),
-    ON(1, "On"),
-    UNDEFINED(-1, "Undefined");
-    private final int value;
-    private final String name;
+  OFF(0, "Off"),
+  ON(1, "On"),
+  UNDEFINED(-1, "Undefined");
+  private final int value;
+  private final String name;
 
-    Control(int value, String name) {
-        this.value = value;
-        this.name = name;
-    }
+  Control(int value, String name) {
+    this.value = value;
+    this.name = name;
+  }
 
-    @Override
-    public int getValue() {
-        return this.value;
-    }
+  @Override
+  public int getValue() {
+    return this.value;
+  }
 
-    @Override
-    public String getName() {
-        return this.name;
-    }
+  @Override
+  public String getName() {
+    return this.name;
+  }
 
-    @Override
-    public OptionsEnum getUndefined() {
+  @Override
+  public OptionsEnum getUndefined() {
+    return UNDEFINED;
+  }
+
+  public static final ElementToChannelConverter ACTIVE_POWER_CONTROL_CONVERTER = new ElementToChannelConverter(//
+      // element -> channel
+      value -> {
+        if (ON.equals(value)) {
+          return DefaultSunSpecModel.S704_WSetEna.ENABLED;
+        }
+        if (OFF.equals(value)) {
+          return DefaultSunSpecModel.S704_WSetEna.DISABLED;
+        }
+        return DefaultSunSpecModel.S704_WSetEna.UNDEFINED;
+      }, //
+      // channel -> element
+      value -> {
+        if (DefaultSunSpecModel.S704_WSetEna.ENABLED.equals(value)) {
+          return ON;
+        }
+        if (DefaultSunSpecModel.S704_WSetEna.DISABLED.equals(value)) {
+          return OFF;
+        }
         return UNDEFINED;
-    }
+      });
 
-    public static final ElementToChannelConverter ACTIVE_POWER_CONTROL_CONVERTER = new ElementToChannelConverter(//
-            // element -> channel
-            value -> {
-                if (ON.equals(value)) return DefaultSunSpecModel.S704_WSetEna.ENABLED;
-                if (OFF.equals(value)) return DefaultSunSpecModel.S704_WSetEna.DISABLED;
-                return DefaultSunSpecModel.S704_WSetEna.UNDEFINED;
-            }, //
-            // channel -> element
-            value -> {
-                if (DefaultSunSpecModel.S704_WSetEna.ENABLED.equals(value)) return ON;
-                if (DefaultSunSpecModel.S704_WSetEna.DISABLED.equals(value)) return OFF;
-                return UNDEFINED;
-            });
-
-    public static final ElementToChannelConverter REACTIVE_POWER_CONTROL_CONVERTER = new ElementToChannelConverter(//
-            // element -> channel
-            value -> {
-                if (ON.equals(value)) return DefaultSunSpecModel.S704_VarSetEna.ENABLED;
-                if (OFF.equals(value)) return DefaultSunSpecModel.S704_VarSetEna.DISABLED;
-                return DefaultSunSpecModel.S704_VarSetEna.UNDEFINED;
-            }, //
-            // channel -> element
-            value -> {
-                if (DefaultSunSpecModel.S704_VarSetEna.ENABLED.equals(value)) return ON;
-                if (DefaultSunSpecModel.S704_VarSetEna.DISABLED.equals(value)) return OFF;
-                return UNDEFINED;
-            });
+  public static final ElementToChannelConverter REACTIVE_POWER_CONTROL_CONVERTER = new ElementToChannelConverter(//
+      // element -> channel
+      value -> {
+        if (ON.equals(value)) {
+          return DefaultSunSpecModel.S704_VarSetEna.ENABLED;
+        }
+        if (OFF.equals(value)) {
+          return DefaultSunSpecModel.S704_VarSetEna.DISABLED;
+        }
+        return DefaultSunSpecModel.S704_VarSetEna.UNDEFINED;
+      }, //
+      // channel -> element
+      value -> {
+        if (DefaultSunSpecModel.S704_VarSetEna.ENABLED.equals(value)) {
+          return ON;
+        }
+        if (DefaultSunSpecModel.S704_VarSetEna.DISABLED.equals(value)) {
+          return OFF;
+        }
+        return UNDEFINED;
+      });
 
 
-    public static final ElementToChannelConverter BATTERY_CONTROL_CONVERTER = new ElementToChannelConverter(//
-            // element -> channel
-            value -> {
-                if (ON.equals(value)) return DefaultSunSpecModel.S802_SetOp.CONNECT;
-                if (OFF.equals(value)) return DefaultSunSpecModel.S802_SetOp.DISCONNECT;
-                return DefaultSunSpecModel.S802_SetOp.UNDEFINED;
-            }, //
-            // channel -> element
-            value -> {
-                if (DefaultSunSpecModel.S802_SetOp.CONNECT.equals(value)) return ON;
-                if (DefaultSunSpecModel.S802_SetOp.DISCONNECT.equals(value)) return OFF;
-                return UNDEFINED;
-            });
+  public static final ElementToChannelConverter BATTERY_CONTROL_CONVERTER = new ElementToChannelConverter(//
+      // element -> channel
+      value -> {
+        if (ON.equals(value)) {
+          return DefaultSunSpecModel.S802_SetOp.CONNECT;
+        }
+        if (OFF.equals(value)) {
+          return DefaultSunSpecModel.S802_SetOp.DISCONNECT;
+        }
+        return DefaultSunSpecModel.S802_SetOp.UNDEFINED;
+      }, //
+      // channel -> element
+      value -> {
+        if (DefaultSunSpecModel.S802_SetOp.CONNECT.equals(value)) {
+          return ON;
+        }
+        if (DefaultSunSpecModel.S802_SetOp.DISCONNECT.equals(value)) {
+          return OFF;
+        }
+        return UNDEFINED;
+      });
 }
--- io.openems.edge.ess.socomec/src/io/openems/edge/ess/socomec/SetOperation.java
+++ io.openems.edge.ess.socomec/src/io/openems/edge/ess/socomec/SetOperation.java
@@ -5,49 +5,66 @@ import io.openems.edge.bridge.modbus.api.ElementToChannelConverter;
 import io.openems.edge.bridge.modbus.sunspec.DefaultSunSpecModel;
 
 public enum SetOperation implements OptionsEnum {
-    STOP(0, "Stop"),
-    START(1, "Start"),
-    EXIT_STANDBY(3, "Exit Standby"),
-    ENTER_STANDBY(2, "Enter Standby"),
-    UNDEFINED(-1, "Undefined");
+  STOP(0, "Stop"),
+  START(1, "Start"),
+  EXIT_STANDBY(3, "Exit Standby"),
+  ENTER_STANDBY(2, "Enter Standby"),
+  UNDEFINED(-1, "Undefined");
 
-    private final int value;
-    private final String name;
-    SetOperation(int value, String name) {
-        this.value = value;
-        this.name = name;
-    }
+  private final int value;
+  private final String name;
 
-    @Override
-    public int getValue() {
-        return this.value;
-    }
+  SetOperation(int value, String name) {
+    this.value = value;
+    this.name = name;
+  }
 
-    @Override
-    public String getName() {
-        return this.name;
-    }
+  @Override
+  public int getValue() {
+    return this.value;
+  }
 
-    @Override
-    public OptionsEnum getUndefined() {
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public OptionsEnum getUndefined() {
+    return UNDEFINED;
+  }
+
+  public static final ElementToChannelConverter SET_OPERATION_CONTROL_CONVERTER = new ElementToChannelConverter(//
+      // element -> channel
+      value -> {
+          if (START.equals(value)) {
+              return DefaultSunSpecModel.S715_OpCtl.START;
+          }
+          if (STOP.equals(value)) {
+              return DefaultSunSpecModel.S715_OpCtl.STOP;
+          }
+          if (ENTER_STANDBY.equals(value)) {
+              return DefaultSunSpecModel.S715_OpCtl.ENTER_STANDBY;
+          }
+          if (EXIT_STANDBY.equals(value)) {
+              return DefaultSunSpecModel.S715_OpCtl.EXIT_STANDBY;
+          }
+        return DefaultSunSpecModel.S715_OpCtl.UNDEFINED;
+      }, //
+      // channel -> element
+      value -> {
+          if (DefaultSunSpecModel.S715_OpCtl.START.equals(value)) {
+              return START;
+          }
+          if (DefaultSunSpecModel.S715_OpCtl.STOP.equals(value)) {
+              return STOP;
+          }
+          if (DefaultSunSpecModel.S715_OpCtl.ENTER_STANDBY.equals(value)) {
+              return ENTER_STANDBY;
+          }
+          if (DefaultSunSpecModel.S715_OpCtl.EXIT_STANDBY.equals(value)) {
+              return EXIT_STANDBY;
+          }
         return UNDEFINED;
-    }
-
-    public static final ElementToChannelConverter SET_OPERATION_CONTROL_CONVERTER = new ElementToChannelConverter(//
-            // element -> channel
-            value -> {
-                if (START.equals(value)) return DefaultSunSpecModel.S715_OpCtl.START;
-                if (STOP.equals(value)) return DefaultSunSpecModel.S715_OpCtl.STOP;
-                if (ENTER_STANDBY.equals(value)) return DefaultSunSpecModel.S715_OpCtl.ENTER_STANDBY;
-                if (EXIT_STANDBY.equals(value)) return DefaultSunSpecModel.S715_OpCtl.EXIT_STANDBY;
-                return DefaultSunSpecModel.S715_OpCtl.UNDEFINED;
-            }, //
-            // channel -> element
-            value -> {
-                if (DefaultSunSpecModel.S715_OpCtl.START.equals(value)) return START;
-                if (DefaultSunSpecModel.S715_OpCtl.STOP.equals(value)) return STOP;
-                if (DefaultSunSpecModel.S715_OpCtl.ENTER_STANDBY.equals(value)) return  ENTER_STANDBY;
-                if (DefaultSunSpecModel.S715_OpCtl.EXIT_STANDBY.equals(value)) return EXIT_STANDBY;
-                return UNDEFINED;
-            });
+      });
 }
